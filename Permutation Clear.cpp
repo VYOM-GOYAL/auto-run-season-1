4f8ee316518a1cf82d698470e857320835206dc2
Vyom Goyal <115768839+VYOM-GOYAL@users.noreply.github.com>
2023-01-07 18:14:24 +0530

    Update Permutation Clear.cpp

--- Permutation Clear.cpp
+++ Permutation Clear.cpp
@@ -49,3 +49,52 @@ int main()
     }
     return 0;
 }
+
+
+
+
+
+//another method of solving this question(which is corect)
+
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t;
+    cin>>t;
+    
+    while(t--)
+    {
+        int n, k;
+        cin>>n;
+        int A[n];
+        map<int, int>mp;
+        for(int i=0;i<n;i++)
+        {
+            cin>>A[i];
+            mp[A[i]]++;
+        }
+        cin>>k;
+        int B[k];
+        for(int i=0;i<k;i++)
+        {
+            cin>>B[i];
+            mp[B[i]]=0;
+        }
+        
+        for(int i=0;i<n;i++)
+        {
+            if(mp[A[i]]>0)
+            {
+                cout<<A[i]<<" ";
+            }
+        }
+        cout<<endl;
+    }
+    return 0;
+}
